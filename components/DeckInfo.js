2979b23d5629dbd5e565e199721ee62810362e15
Eron Castro <eroncastro@gmail.com>
2019-06-26 22:40:47 -0300

    Added DeckInfo

---
+++ components/DeckInfo.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function DeckInfo(props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{props.deckName}</Text>
+      <Text style={styles.subtitle}>{props.cards} cards</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    // flex: 1,
+    fontSize: 30,
+    padding: 20
+  },
+  subtitle: {
+    // flex: 1,
+    fontSize: 15,
+    padding: 20
+  }
+});
