f9a0be39ff3d6d7569dcf3639d94a0b5e568ae3f
Eron Castro <eroncastro@gmail.com>
2019-07-02 10:02:22 -0300

    Removed console.log

--- src/middleware/async_storage_sync.js
+++ src/middleware/async_storage_sync.js
@@ -1,10 +1,7 @@
 import { saveDecks } from '../utils/storage';
 
 export const asyncStorageSync = store => next => action => {
-  console.log(store.getState());
   const result = next(action);
-  console.log(store.getState());
-
   const { decks } = store.getState();
 
   saveDecks(decks);
